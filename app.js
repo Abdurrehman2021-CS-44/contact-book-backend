3b6d299eecb9739c2ae5af0f744993aa00601b11
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-08-03 16:36:47 +0500

    Setup database

--- app.js
+++ app.js
@@ -6,10 +6,28 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://127.0.0.1:27017/contactsDB")
+.then(()=>{
+    console.log("Database connected");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+const contactSchema = new mongoose.Schema({
+    fName: String,
+    lName: String,
+    email: String,
+    phoneNum: String,
+    address: String
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
 app.get("/contacts", (req, res) => {
     res.json({name: "Abdurrehman Bin Faheem"})
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
+app.listen(5000, () => {
+    console.log("Server is running on port 5000.");
 })
